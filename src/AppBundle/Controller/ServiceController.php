98403d2b9f45ffa7a13f6d1d16410512782037f6
Akriti3011 <akriti.bhadoriya96@gmail.com>
2017-11-10 02:38:23 +0530

    services functions added.

--- src/AppBundle/Controller/ServiceController.php
+++ src/AppBundle/Controller/ServiceController.php
@@ -10,6 +10,10 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\customerDetail;
 use AppBundle\Entity\customer;
+use AppBundle\Entity\meter;
+use AppBundle\Entity\source;
+use AppBundle\Entity\production;
+use AppBundle\Entity\Bill;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -86,7 +90,50 @@ class ServiceController extends Controller
      */
     public function trackAction(){
 
-        return $this->render('/Services/track.html.twig');
+        if (!empty($_REQUEST['areaCode'])) {
+            $areaCode = $_POST['areaCode'];
+            $productionlist = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('AppBundle:production')
+                ->findBy(array("areaCode" => $areaCode));
+
+            if (isset($productionlist) && !empty($productionlist)) {
+                ?>
+                <table id="table1" class="table table-responsive">
+                    <thead>
+                    <tr>
+                        <th>Source</th>
+                        <th>Electricity Produced</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($productionlist as $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value->getElecSource();?></td>
+                            <td><?php echo $value->getElecProduced();?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }
+            else {
+                ?>
+                <div class="container table-responsive" id="report_tbl">
+                    <h4 class="alert-info" style="padding: 15px;">No Records Found !</h4>
+                </div>
+                <?php
+
+            }
+            return new Response();
+        }
+        else {
+            return $this->render('/Services/track.html.twig');
+        }
     }
 
     /**
@@ -94,7 +141,49 @@ class ServiceController extends Controller
      */
     public function bestSourceAction(){
 
-        return $this->render('/Services/bestSource.html.twig');
+        if (!empty($_REQUEST['areaCode'])) {
+            $areaCode = $_POST['areaCode'];
+            $weatherlist = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('AppBundle:weather')
+                ->findBy(array("areaCode" => $areaCode));
+            if (isset($weatherlist) && !empty($weatherlist)) {
+                ?>
+                <table id="table1" class="table table-responsive">
+                    <thead>
+                    <tr>
+                        <th>Weather</th>
+                        <th>Suitable Source</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($weatherlist as $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value->getType();?></td>
+                            <td><?php echo $value->getBestSource();?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }
+            else {
+                ?>
+                <div class="container table-responsive" id="report_tbl">
+                    <h4 class="alert-info" style="padding: 15px;">No Records Found !</h4>
+                </div>
+                <?php
+
+            }
+            return new Response();
+        }
+        else {
+            return $this->render('/Services/bestSource.html.twig');
+        }
     }
 
     /**
@@ -102,47 +191,46 @@ class ServiceController extends Controller
      */
     public function billAction(){
 
-
         if (!empty($_REQUEST['meterId'])){
             $meterId = $_POST['meterId'];
-            var_dump($meterId);exit;
-//            $bill_list = $this->getDoctrine()
-//                ->getManager()
-//                ->getRepository('AppBundle:source')
-//                ->findBy(array("meterId" => $meterId));
+            $month = $_POST['month'];
+            $year = $_POST['year'];
 
-//            if (isset($bill_list) && !empty($bill_list)) {
-//                ?>
-<!--                <table id="table1" class="table table-responsive">-->
-<!--                    <thead>-->
-<!--                    <tr>-->
-<!--                        <th>Source</th>-->
-<!--                        <th>Rate</th>-->
-<!--                    </tr>-->
-<!--                    </thead>-->
-<!--                    <tbody>-->
-<!--                    --><?php
-//                    foreach($sourcelist as $value) {
-//                        ?>
-<!--                        <tr>-->
-<!--                            <td>--><?php //echo $value->getElecSource();?><!--</td>-->
-<!--                            <td>--><?php //echo $value->getRate();?><!--</td>-->
-<!--                        </tr>-->
-<!--                        --><?php
-//                    }
-//                    ?>
-<!--                    </tbody>-->
-<!--                </table>-->
-<!--                --><?php
-//            }
-//            else {
-//                ?>
-<!--                <div class="container table-responsive" id="report_tbl">-->
-<!--                    <h4 class="alert-info" style="padding: 15px;">No Records Found !</h4>-->
-<!--                </div>-->
-<!--                --><?php
-//
-//            }
+            $bill_list = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('AppBundle:Bill')
+                ->findBy(array("meterId" => $meterId,"billMonth" => $month,"billYear"=> $year));
+            if (isset($bill_list) && !empty($bill_list)) {
+                ?>
+                <table id="table1" class="table table-responsive">
+                    <thead>
+                    <tr>
+                        <th>Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($bill_list as $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value->getBillAmount();?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }
+            else {
+                ?>
+                <div class="container table-responsive" id="report_tbl">
+                    <h4 class="alert-info" style="padding: 15px;">No Records Found !</h4>
+                </div>
+                <?php
+
+
+            }
             return new Response();
         }
         else{
@@ -157,7 +245,19 @@ class ServiceController extends Controller
      */
     public function liveAction(){
 
-        return $this->render('/Services/live.html.twig');
+        $customer_id=($this->get('session')->get('customer_id'));
+        //var_dump($customer_id);exit;
+        if($customer_id!=null)
+        {
+            $customerDetail = $this->getDoctrine()->getManager()->getRepository('AppBundle:customerDetail')->findOneBy(array("customer_id" =>  $customer_id));
+            $meterId = $customerDetail->getMeterId();
+            $meterDetail = $this->getDoctrine()->getManager()->getRepository('AppBundle:Meter')->findOneBy(array("meterId" =>  $meterId));
+            return $this->render('/Services/live.html.twig',array('meterDetail'=>$meterDetail));
+
+        }
+        else{
+            return $this->render('/Services/live.html.twig');
+        }
     }
 
     /**
@@ -165,7 +265,50 @@ class ServiceController extends Controller
      */
     public function wastageAction(){
 
-        return $this->render('/Services/wastage.html.twig');
+        if (!empty($_REQUEST['areaCode'])) {
+            $areaCode = $_POST['areaCode'];
+            $productionlist = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('AppBundle:production')
+                ->findBy(array("areaCode" => $areaCode));
+
+            if (isset($productionlist) && !empty($productionlist)) {
+                ?>
+                <table id="table1" class="table table-responsive">
+                    <thead>
+                    <tr>
+                        <th>Source</th>
+                        <th>Electricity Wasted</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($productionlist as $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value->getElecSource();?></td>
+                            <td><?php echo $value->getElecWastage();?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }
+            else {
+                ?>
+                <div class="container table-responsive" id="report_tbl">
+                    <h4 class="alert-info" style="padding: 15px;">No Records Found !</h4>
+                </div>
+                <?php
+
+            }
+            return new Response();
+        }
+        else {
+            return $this->render('/Services/wastage.html.twig');
+        }
     }
 
 }
